bd95e4e7f7ad42d132dc67263da20874f2aa3442
volodymyr198 <107930588+volodymyr198@users.noreply.github.com>
2023-02-22 17:24:33 +0000

    Deploying to gh-pages from @ volodymyr198/goit-react-hw-08-phonebook@a9c8b90291c657461d0069380ddc0673e7ab66fe 🚀

---
+++ static/js/794.a05fa71b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[794],{1794:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"HomePage"})}}}]);
+//# sourceMappingURL=794.a05fa71b.chunk.js.map
